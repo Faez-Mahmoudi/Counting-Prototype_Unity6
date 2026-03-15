ead61b615786e25549786bd47b9103fc9768ef78
Faez Mahmoudi <faez.mahmudi.1373@gmail.com>
2026-03-15 05:55:10 -0700

    PlayerController script p2
    Replace AddForce method instead of Transform method to control the player base on Physics laws

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,11 +2,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float speed = 5.0f;
+    [SerializeField] private float speed = 1000.0f;
+    private Rigidbody playerRb;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();   
     }
 
     // Update is called once per frame
@@ -15,7 +17,12 @@ public class PlayerController : MonoBehaviour
         float verticalInput = Input.GetAxis("Vertical");
         float horizontalInput = Input.GetAxis("Horizontal");
 
-        transform.Translate(Vector3.up * Time.deltaTime * speed * verticalInput);
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * horizontalInput);
+        playerRb.AddForce(Vector3.up * Time.deltaTime * speed * verticalInput);
+        playerRb.AddForce(Vector3.forward * Time.deltaTime * speed * horizontalInput);
+
+        transform.rotation = Quaternion.identity;
+
+        //transform.Translate(Vector3.up * Time.deltaTime * speed * verticalInput);
+        //transform.Translate(Vector3.forward * Time.deltaTime * speed * horizontalInput);
     }
 }
