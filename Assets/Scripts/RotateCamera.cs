f8b9ff69135dfa51f0c4d46bc5edf491b341b712
Faez Mahmoudi <faez.mahmudi.1373@gmail.com>
2026-05-24 11:53:22 -0700

    Game and Scene Design p21
    Update RotateCamera.cs to change camera view base on the ball movement

--- Assets/Scripts/RotateCamera.cs
+++ Assets/Scripts/RotateCamera.cs
@@ -2,18 +2,19 @@ using UnityEngine;
 
 public class RotateCamera : MonoBehaviour
 {
-    [SerializeField] private float rotationSpeed = 50.0f;
+    //[SerializeField] private float rotationSpeed = 5.0f;
+    private GameObject ball;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        ball = GameObject.Find("Ball");
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal1");
-        transform.Rotate(Vector3.up, horizontalInput * rotationSpeed * Time.deltaTime);
+        float ballPositionZ = ball.transform.position.z / 180.0f;
+        gameObject.transform.rotation = new Quaternion(0, ballPositionZ, 0, 1);
     }
 }
