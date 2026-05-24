a16f3e0aa89170a3bca5900ae6f94293533e05db
Faez Mahmoudi <faez.mahmudi.1373@gmail.com>
2026-05-24 10:42:58 -0700

    Game and Scene Design p20
    Create a focal point game object and a RotateCamera.cs to control camera view

---
+++ Assets/Scripts/RotateCamera.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+    [SerializeField] private float rotationSpeed = 50.0f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal1");
+        transform.Rotate(Vector3.up, horizontalInput * rotationSpeed * Time.deltaTime);
+    }
+}
