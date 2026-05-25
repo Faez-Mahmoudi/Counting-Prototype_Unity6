211a41283b00b31563646d84af1306182dc4d993
Faez Mahmoudi <faez.mahmudi.1373@gmail.com>
2026-05-25 01:11:32 -0700

    Game and Scene Design p22
    Update RotateCamera.cs to behave better on ball chasing

--- Assets/Scripts/RotateCamera.cs
+++ Assets/Scripts/RotateCamera.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 public class RotateCamera : MonoBehaviour
 {
-    //[SerializeField] private float rotationSpeed = 5.0f;
     private GameObject ball;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -12,9 +11,9 @@ public class RotateCamera : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        float ballPositionZ = ball.transform.position.z / 180.0f;
+        float ballPositionZ = ball.transform.position.z / 240.0f;
         gameObject.transform.rotation = new Quaternion(0, ballPositionZ, 0, 1);
     }
 }
